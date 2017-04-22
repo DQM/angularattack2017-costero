aa436f027cb8654554d5001b4df6717464e07c6d
Vandré Leal Cândido <vleal@uvsouthsourcing.com>
2017-04-22 13:12:50 -0300

    Code formatted.

--- src/app/core/location.class.ts
+++ src/app/core/location.class.ts
@@ -2,8 +2,8 @@ import {ILatLng} from './latLng.interface';
 import {LngLatBounds} from 'mapbox-gl';
 
 export class Location implements ILatLng {
-    latitude: number;
-    longitude: number;
-    address: string;
-    viewBounds: LngLatBounds;
+  latitude: number;
+  longitude: number;
+  address: string;
+  viewBounds: LngLatBounds;
 }
