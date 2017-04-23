31d59ebb51ccdfc2e3d1cd9ae66a8039d8bdf41a
Emerson Jair <emersonjairr@gmail.com>
2017-04-23 02:54:25 -0300

    Add top issues list

--- src/app/components/map-sidebar/map-sidebar.component.ts
+++ src/app/components/map-sidebar/map-sidebar.component.ts
@@ -10,10 +10,12 @@ import { DataApiService } from '../../services/data-api.service';
 })
 export class MapSidebarComponent implements OnInit {
   private recentIssues: any;
+  private topIssues: any;
   private addReport: boolean = false;
 
   constructor(private data: DataApiService) {
     this.recentIssues = this.data.getRecentIssues();
+    this.topIssues = this.data.getTopIssues();
   }
 
   ngOnInit() { }
