39cd705794a0d24a4ca7674fb99156a4a008f644
Gustavo Sampaio <gbritosampaio@gmail.com>
2017-04-23 02:14:00 -0300

    Refactoring routes

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -17,67 +17,9 @@ import { LngLat, Map } from 'mapbox-gl';
 export class AppComponent {
   title = 'app works!';
 
-  private issuesLocationQuery: any;
-  private topIssues: any;
+  constructor() {
 
-  constructor(private mapService: MapService, private data: DataApiService, private geocoding: GeocodingService, private auth: AuthService) {
 
-    this.auth.getUser().subscribe(
-      user => {
-        if(!user) return;
-
-        let issue = new Issue();
-        issue.title = 'geo of ' + user.displayName;
-        issue.lat = -7.140000 + ( (Math.random()%2 == 0 ? 1 : -1) * (Math.random() % 10) / 100 );
-        issue.long = -34.851654 + ((Math.random() % 2 == 0 ? 1 : -1) * (Math.random() % 10) / 100);
-        issue.author = user.uid;
-        // data.addIssue(issue);
-
-      }
-    );
-
-    // this.issuesLocationQuery = this.data.getIssuesAround([-8.0431353, -35.0062387], 5);
-
-    this.topIssues = this.data.getTopIssues();
-
-    geocoding.getCurrentLocation().subscribe(v => mapService.setCurrentPosition(v.longitude, v.latitude), err => console.log(err), () => console.log('completed') );
-
-  }
-
-  onButtonClick() {
-    this.title = 'Hello from Kendo UI';
-  }
-
-  selectFile(event) {
-    let file = event.srcElement.files[0];
-    console.log(file);
-    let uploadTask: UploadTask = this.data.uploadPhoto(file, file.type);
-    uploadTask.task.subscribe(state => {
-
-      // state: {
-      //  downloadUrl: string,
-      //  progress: number
-      // }
-      //
-      console.log(state);
-
-    }, error => {
-      console.log(error);
-    }, () => {
-      console.log('complete. Url: ' + uploadTask.downloadURL);
-    });
-  }
-
-  login() {
-    this.auth.login();
-  }
-
-  logout() {
-    this.auth.logout();
-  }
-
-  get user() {
-    return this.auth.getUser();
   }
 
 }
