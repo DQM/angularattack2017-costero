e51c1f8ce8f193a5cbc7182755b430873746e20f
Gustavo Sampaio <gbritosampaio@gmail.com>
2017-04-23 17:40:29 -0300

    Sidebar: Added control to like/dislike button

--- src/app/components/issue-list-item/issue-list-item.component.ts
+++ src/app/components/issue-list-item/issue-list-item.component.ts
@@ -14,14 +14,33 @@ export class IssueListItemComponent implements OnInit {
   @Input() issue: Issue;
 
   private likes: Observable<number> = Observable.of(0);
+  private hasLiked: Observable<boolean> = Observable.of(false);
 
   constructor(private data: DataApiService,) {
+
 
   }
 
   ngOnInit() {
     // console.log(this.issue);
+    this.hasLiked = this.data.hasLiked((<any>this.issue).$key);
     this.likes = this.data.getTotalLikes((<any> this.issue).$key);
+  }
+
+  toggleLike() {
+
+    this.hasLiked.first().subscribe(
+
+      liked => {
+        if(liked) {
+          this.data.removeLike((<any>this.issue).$key).then().catch(console.log);
+        } else {
+          this.data.performLike((<any>this.issue).$key).then().catch(console.log);;
+        }
+      }
+
+    );
+
   }
 
 }
